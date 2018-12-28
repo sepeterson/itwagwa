be2baacb9c27e588ccab8995347e593157bb29d3
Seth Peterson <seth@sethpeterson.net>
2018-12-28 15:37:36 -0500

    add some padding around tiles

--- src/MainScreen.js
+++ src/MainScreen.js
@@ -3,27 +3,53 @@
  */
 
 import React, { Component } from "react";
-import { StyleSheet, Text, View, ScrollView, RefreshControl } from "react-native";
-import WeatherTile from './WeatherTile';
+import {
+  StyleSheet,
+  Text,
+  View,
+  ScrollView,
+  RefreshControl
+} from "react-native";
+import WeatherTile from "./WeatherTile";
 import { connect } from "react-redux";
 import * as actions from "./state/actions";
 import * as selectors from "./state/selectors";
 import apiKey from "../apiKey.json";
 import type { HourlyData } from "./state/types";
 
-type Props = { getWeather: () => void, fetching: boolean, currentData?: HourlyData, };
+type Props = {
+  getWeather: () => void,
+  fetching: boolean,
+  currentData?: HourlyData
+};
 class MainScreen extends Component<Props> {
+  renderNoData() {
+    if (!this.props.currentData) {
+      return (
+        <View style={styles.noDataContainer}>
+          <Text style={styles.noDataText}>Pull down to fetch weather data!</Text>
+        </View>
+      );
+    }
+  }
 
   render() {
     return (
-      <ScrollView style={styles.container} refreshControl={
+      <ScrollView
+        style={styles.container}
+        refreshControl={
           <RefreshControl
             refreshing={this.props.fetching}
             onRefresh={this.props.getWeather}
           />
-        }>
-        {this.props.currentData && <WeatherTile weatherData={this.props.currentData} />}
-        {this.props.fetching && <Text>FETCHING!!!!</Text>}
+        }
+      >
+        {this.props.currentData && (
+          <View style={styles.tileContainer}>
+            <WeatherTile weatherData={this.props.currentData} />
+          </View>
+        )}
+        {this.renderNoData()}
       </ScrollView>
     );
   }
@@ -33,13 +59,23 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#565c60"
+  },
+  noDataContainer: {
+    alignItems: "center",
+    paddingTop: 32
+  },
+  noDataText: {
+    color: 'white',
+  },
+  tileContainer: {
+    padding: 12,
   }
 });
 
 const mapStateToProps = state => {
   return {
     fetching: selectors.getFetching(state),
-    currentData: selectors.getCurrentData(state),
+    currentData: selectors.getCurrentData(state)
   };
 };
 
--- src/WeatherTile.js
+++ src/WeatherTile.js
@@ -7,18 +7,31 @@ import type { HourlyData } from "./state/types";
 
 type Props = { weatherData: HourlyData };
 export default class WeatherTile extends Component<Props> {
-
   render() {
     const data = this.props.weatherData;
     return (
-      <View style={styles.container} >
-        <Text style={[styles.smallText, styles.allText]}>{Moment().format('MMMM Do, h:mm a')}</Text>
-        <Text style={[styles.largeText, styles.allText]}>{data.summary}</Text>
-        <Text style={[styles.largeText, styles.allText]}>{`${data.temperature}°`}</Text>
-        <Text style={[styles.smallText, styles.allText]}>Feels like: {data.apparentTemperature}°</Text>
-        <Text style={[styles.smallText, styles.allText]}>Dew point: {data.dewPoint}°</Text>
-        <Text style={[styles.smallText, styles.allText]}>Humidity: {data.humidity*100}%</Text>
-        <Text style={[styles.smallText, styles.allText]}>Wind speed: {data.windSpeed} mph</Text>
+      <View style={styles.container}>
+        <View style={styles.textContainer}>
+          <Text style={[styles.smallText, styles.allText]}>
+            {Moment().format("MMMM Do, h:mm a")}
+          </Text>
+          <Text style={[styles.largeText, styles.allText]}>{data.summary}</Text>
+          <Text style={[styles.largeText, styles.allText]}>{`${
+            data.temperature
+          }°`}</Text>
+          <Text style={[styles.smallText, styles.allText]}>
+            Feels like: {data.apparentTemperature}°
+          </Text>
+          <Text style={[styles.smallText, styles.allText]}>
+            Dew point: {data.dewPoint}°
+          </Text>
+          <Text style={[styles.smallText, styles.allText]}>
+            Humidity: {data.humidity * 100}%
+          </Text>
+          <Text style={[styles.smallText, styles.allText]}>
+            Wind speed: {data.windSpeed} mph
+          </Text>
+        </View>
       </View>
     );
   }
@@ -29,12 +42,15 @@ const styles = StyleSheet.create({
     backgroundColor: "#F5FCFF"
   },
   largeText: {
-    fontSize: 22,
+    fontSize: 22
   },
   smallText: {
-    fontSize: 12,
+    fontSize: 12
   },
   allText: {
-    color: 'black',
+    color: "black"
+  },
+  textContainer: {
+    padding: 8
   }
 });
